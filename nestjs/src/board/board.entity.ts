55c8630afc84ef918079ffde2c1d67028d8400ea
Kyle Saunders <kyle.saunders1310@gmail.com>
2023-01-22 16:30:23 +0200

    Create Board entity

---
+++ nestjs/src/board/board.entity.ts
@@ -0,0 +1,24 @@
+import {Column, Entity, ObjectIdColumn} from "typeorm";
+import {Member} from "./interfaces/member.interface";
+import {ConfigOption} from "./interfaces/config-option.interface";
+
+@Entity()
+export class Board {
+    @ObjectIdColumn()
+    _id: string
+
+    @Column()
+    name: string
+
+    @Column()
+    members: Member[]
+
+    @Column()
+    statuses: ConfigOption[]
+
+    @Column()
+    categories: ConfigOption[]
+
+    @Column()
+    priorities: ConfigOption[]
+}
