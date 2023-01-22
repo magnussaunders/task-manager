19a3f1a479dcc4958d12d8a218d1ae3d2a9d1988
Kyle Saunders <kyle.saunders1310@gmail.com>
2023-01-22 21:20:30 +0200

    Complete CRUD functionality for Board

---
+++ nestjs/src/board/board.controller.ts
@@ -0,0 +1,33 @@
+import {Body, Controller, Delete, Get, Param, Patch, Put} from '@nestjs/common';
+import {BoardService} from "./board.service";
+import {Board} from "./board.entity";
+
+@Controller('board')
+export class BoardController {
+    constructor(private boardService: BoardService) {}
+
+    @Get()
+    getAllBoards(): Promise<Board[]> {
+        return this.boardService.findAll()
+    }
+
+    @Get(':boardId')
+    getBoardById(@Param() params): Promise<Board> {
+        return this.boardService.findById(params.boardId)
+    }
+
+    @Put()
+    createBoard(@Body() board: Board): Promise<Board> {
+        return this.boardService.create(board)
+    }
+
+    @Patch()
+    updateBoard(@Body() board: Board): Promise<Board> {
+        return this.boardService.update(board)
+    }
+
+    @Delete()
+    deleteBoard(@Body() board: Board): Promise<void> {
+        return this.boardService.delete(board)
+    }
+}
--- nestjs/src/board/board.module.ts
+++ nestjs/src/board/board.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Board} from "./board.entity";
 import { BoardService } from './board.service';
+import { BoardController } from './board.controller';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Board])],
-    providers: [BoardService]
+    providers: [BoardService],
+    controllers: [BoardController]
 })
 export class BoardModule {}
