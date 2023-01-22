498b0c38758b75d37fb8ed62ed1c8342d6177c0e
Kyle Saunders <kyle.saunders1310@gmail.com>
2023-01-22 16:40:18 +0200

    Create Board service

--- nestjs/src/board/board.module.ts
+++ nestjs/src/board/board.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Board} from "./board.entity";
+import { BoardService } from './board.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Board])]
+    imports: [TypeOrmModule.forFeature([Board])],
+    providers: [BoardService]
 })
 export class BoardModule {}
