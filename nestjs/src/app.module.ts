5da03cfd34fd5fdb4506a60687e8c43a9b626449
Kyle Saunders <kyle.saunders1310@gmail.com>
2023-01-22 05:21:02 +0200

    Create User module

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -14,7 +15,8 @@ import {TypeOrmModule} from "@nestjs/typeorm";
       database: 'demo-api-db',
       entities: [],
       synchronize: true,
-    })
+    }),
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
