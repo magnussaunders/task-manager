4ef8f94e9ef5fb3a44e5d33722d7930b062571dc
Kyle Saunders <kyle.saunders1310@gmail.com>
2023-01-23 03:44:52 +0200

    Implement env variables

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -13,11 +13,11 @@ import {Task} from "./task/task.entity";
   imports: [
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      host: 'localhost',
-      port: 27017,
-      username: 'nestjs',
-      password: 'thisisNest123',
-      database: 'demo-api-db',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DB,
       entities: [Board, User, Task],
       synchronize: true,
     }),
